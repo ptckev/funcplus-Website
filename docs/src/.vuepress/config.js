a7a0fc378bc1190ddfae79056df55d16ad617a13
77455257 <77455257+OskarP27@users.noreply.github.com>
2021-08-03 17:04:01 +0100

    master

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,80 @@
+const { description } = require('../../package')
+
+module.exports = {
+  theme: 'yuu',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'funcplus.js',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    yuu: {
+			defaultDarkTheme: true,
+		},
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Documentation',
+        link: '/docs/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/ptckev/funcplus.js'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/docs/': [
+        {
+          title: 'Getting Started',
+          collapsable: false,
+          children: [
+            'Installation',
+            'Setup'
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    '@vuepress/nprogress',
+    '@vuepress/search',
+    '@vuepress/active-header-links',
+    '@vuepress/register-components',
+    'container',
+    'vuepress-theme-yuu'
+  ]
+}
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://i.imgur.com/Ol1iawe.png
+tagline: 
+actionText: Quick Start →
+actionLink: /docs/
+features:
+- title: Feature 1 Titles
+  details: Feature 1 Description
+- title: Feature 2 Title
+  details: Feature 2 Description
+- title: Feature 3 Title
+  details: Feature 3 Description
+footer: Made by  with ❤️
+---
